235021cfb599bf4e399240d071ffcc629cfbce99
Chris Hamons <chris.hamons@microsoft.com>
2021-03-26 15:08:38 -0500

    Example

---
+++ Library.cs
@@ -0,0 +1,34 @@
+using System.Threading;
+using Azure;
+using Azure.Core;
+
+namespace Foo.Bar
+{
+    public class OptionsBag 
+    {
+        CancellationToken CancellationToken;
+        public OptionsBag ()
+        {
+            CancellationToken = default;
+        }
+    }
+
+    public class ThingResource
+    {
+
+    }
+
+    public class FooClient
+    {
+        public FooClient ()
+        {
+
+        }
+        
+        public Response DoThing (RequestContent body, OptionsBag options = default) => null;
+        public Response CheckThing (OptionsBag options = default) => null;
+
+        public Response<ThingResource> DoThing (ThingResource resource, CancellationToken cancellationToken = default) => null;
+        public Response<bool> CheckThing (CancellationToken cancellationToken = default) => null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Foo.Bar;
+using Azure;
+using Azure.Core;
+
+namespace Foo.Bar.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            FooClient client = new FooClient ();
+
+            client.DoThing (RequestContent.Create(""));
+
+            client.CheckThing ();
+        }
+    }
+}
